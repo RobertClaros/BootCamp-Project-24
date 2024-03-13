206ce09a3d38008bf15521d097483833e6ae1b27
RobertClaros <audioprobolivia@gmail.com>
2024-03-13 05:53:22 -0400

    Integrate ESLint for code quality and style consistency

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
-import express, { Request, Response } from 'express';
-import { v4 as uuidv4} from 'uuid';
+import express, { Request, Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const secrets: { [key: string]: string} ={};
-app.use(express.json());
+const secrets: { [key: string]: string} = {}
+app.use(express.json())
 
 app.post('/api/secrets', (req: Request, res: Response) => {
-    const { message } = req.body;
-    const secretKey = generateSecretKey(); 
-    secrets[secretKey] = message;
-    res.status(200).json({ secretKey });
-});
+  const { message } = req.body
+  const secretKey = generateSecretKey()
+  secrets[secretKey] = message
+  res.status(200).json({ secretKey })
+})
 
 app.get('/api/secret/:secretKey', (req: Request, res: Response) => {
-    const { secretKey } = req.params;
-    const secretMessage = secrets[secretKey];
-    if (secretMessage) {
-        delete secrets[secretKey];
-        res.json({ secretMessage });
-    } else {
-        res.status(404).json({ error: 'Secret not found' });
-    }
-});
+  const { secretKey } = req.params
+  const secretMessage = secrets[secretKey]
+  if (secretMessage) {
+    delete secrets[secretKey]
+    res.json({ secretMessage })
+  } else {
+    res.status(404).json({ error: 'Secret not found' })
+  }
+})
 
-function generateSecretKey(): string {
-    return uuidv4(); 
+function generateSecretKey (): string {
+  return uuidv4()
 }
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
